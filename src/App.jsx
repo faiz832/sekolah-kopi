f0099711dda5ce3330afb6c91bfb999d2cf747a7
faiz832 <faqodefazz@gmail.com>
2025-06-19 12:25:20 +0700

    add template layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import "./App.css";
+
 export default function App() {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
