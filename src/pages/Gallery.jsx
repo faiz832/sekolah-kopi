5fd3fa9561a6e9bc16af12df44a0b416d3b6b0a3
faiz832 <faqodefazz@gmail.com>
2025-06-20 07:36:31 +0700

    fix

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,5 +1,6 @@
 import Pattern1 from "../assets/images/pattern1.svg";
 import Img1 from "../assets/images/img11.png";
+import Img2 from "../assets/images/img12.png";
 
 export default function Gallery() {
   return (
@@ -7,7 +8,7 @@ export default function Gallery() {
       <section className="relative bg-[url('/src/assets/images/bg-gallery.png')] h-screen bg-cover bg-center bg-no-repeat z-1">
         <div className="flex justify-center items-center h-full w-full px-4">
           <div className="text-white text-center w-full">
-            <h1 className="text-4xl sm:text-6xl uppercase font-bold md:w-4xl mx-auto">GALERY: Momen di sekolah Kopi lampung barat</h1>
+            <h1 className="text-4xl sm:text-6xl uppercase font-bold lg:w-4xl mx-auto">GALERY: Momen di sekolah Kopi lampung barat</h1>
             <p className="text-sm md:text-xl mt-4">Mengenal lebih dekat para petani, siswa, instruktur dan kegiatan yang ada di Sekolah Kopi Lampung Barat.</p>
           </div>
         </div>
@@ -16,16 +17,23 @@ export default function Gallery() {
       </section>
 
       <section className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 md:py-24 z-1">
-        <div className="flex flex-col md:flex-row gap-8 md:gap-24">
+        <div className="flex flex-col md:flex-row gap-8 md:gap-12 lg:gap-24">
           <div className="flex flex-col justify-between gap-8">
-            <h1 className="uppercase text-3xl md:text-4xl font-light text-center md:text-right">Selamat datang di Galeri Sekolah Kopi Lampung Barat!</h1>
-            <p className="md:text-2xl w-full text-justify">
+            <h1 className="uppercase text-3xl md:text-4xl font-light text-center md:text-right tracking-wider">Selamat datang di Galeri Sekolah Kopi Lampung Barat!</h1>
+            <p className="md:text-xl lg:text-2xl w-full text-justify">
               Di sini, Anda dapat menjelajahi berbagai momen edukatif dan menarik yang terjadi di Sekolah Kopi. Setiap gambar dan video kami hadirkan untuk membawa Anda lebih dekat dengan proses pembuatan kopi yang unik, keindahan alam
               Lampung Barat, serta pengalaman wisata edukatif yang kami tawarkan.
             </p>
           </div>
 
-          <img src={Img1} alt="" className="" />
+          <img src={Img1} alt="" className="w-full md:w-1/2 lg:w-full h-auto object-cover" />
+        </div>
+      </section>
+
+      <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-12">
+        <div className="flex flex-col md:flex-row justify-end gap-12">
+          <h1 className="uppercase text-3xl md:text-4xl font-light text-center md:text-right w-full md:w-[370px] tracking-wider">Melihat Lebih Dekat Proses Kopi Lampung Barat</h1>
+          <img src={Img2} alt="" />
         </div>
       </section>
     </>
